a92b586bb295cc61478d1df77e4f8c6cf184ec26
Yukirin <skeith@users.noreply.github.com>
2018-09-18 13:51:23 +0800

    Use d.py nsfw handling instead

--- doujin/doujin.py
+++ doujin/doujin.py
@@ -14,57 +14,37 @@ class Doujin:
     """Doujin commands."""
 
     def __init__(self, bot):
-        self.bot = bot
         self.session = aiohttp.ClientSession()
 
-    @commands.group()
+    @commands.group(autohelp=True)
+    @command.guild_only()
+    @command.is_nsfw()
     async def doujin(self, ctx):
         """Doujin commands"""
         pass
 
-    @doujin.command(no_pm=True)
+    @doujin.command()
     async def nhentai(self, ctx):
         """Sends a random doujin"""
 
-        channel_nsfw = await self.is_nsfw(ctx.message.channel)
-        if not channel_nsfw:
-            return
-        else:
-            url = "http://nhentai.net/random/"
-            async with self.session.get(url) as r:
-                await ctx.send(r.url)
+        url = "http://nhentai.net/random/"
+        async with self.session.get(url) as r:
+            await ctx.send(r.url)
 			
-    @doujin.command(no_pm=True)
+    @doujin.command()
     async def tsumino(self, ctx):
         """Sends a random doujin"""
 
-        channel_nsfw = await self.is_nsfw(ctx.message.channel)
-        if not channel_nsfw:
-            return
-        else:
-            url = "http://www.tsumino.com/Browse/Random"
-            async with self.session.get(url) as r:
-                await ctx.send(r.url)
+        url = "http://www.tsumino.com/Browse/Random"
+        async with self.session.get(url) as r:
+            await ctx.send(r.url)
 			
-    @doujin.command(no_pm=True)
+    @doujin.command()
     async def hbrowse(self, ctx):
         """Sends a random doujin"""
+        
+        url = "http://www.hbrowse.com/random"
+        async with self.session.get(url) as r:
+            await ctx.send(r.url)
 
-        channel_nsfw = await self.is_nsfw(ctx.message.channel)
-        if not channel_nsfw:
-            return
-        else:
-            url = "http://www.hbrowse.com/random"
-            async with self.session.get(url) as r:
-                await ctx.send(r.url)
 
-    async def is_nsfw(self, channel: discord.abc.GuildChannel):
-        try:
-            _gid = channel.guild.id
-        except AttributeError:
-            return False
-        data = await self.bot.http.request(
-            discord.http.Route(
-                'GET', '/guilds/{guild_id}/channels', guild_id=_gid))
-        channeldata = [d for d in data if d['id'] == channel.id][0]
-        return channeldata['nsfw']
