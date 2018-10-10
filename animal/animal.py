ded5ce12435f325477c34687cc004b58eea14f58
Yukirin <skeith@users.noreply.github.com>
2018-10-10 11:44:18 +0800

    Session closing and class override fix
    animal.py = Session closing and class override fix
    doujin.py = Session closing fix

--- animal/animal.py
+++ animal/animal.py
@@ -9,10 +9,10 @@ from redbot.core import commands
 # Libs
 import aiohttp
 
-cat = "http://random.cat/meow"
-dog = "https://dog.ceo/api/breeds/image/random"
-fox = "http://wohlsoft.ru/images/foxybot/randomfox.php"
-pug = "http://pugme.herokuapp.com/random"
+catapi = "http://random.cat/meow"
+dogapi = "https://dog.ceo/api/breeds/image/random"
+foxapi = "http://wohlsoft.ru/images/foxybot/randomfox.php"
+pugapi = "http://pugme.herokuapp.com/random"
 
 BaseCog = getattr(commands, "Cog", object)
 
@@ -21,18 +21,19 @@ class Animal(BaseCog):
     """Animal commands."""
 
     def __init__(self, bot):
-        self.session = aiohttp.ClientSession()
-        self.cat = cat
-        self.dog = dog
-        self.fox = fox
-        self.pug = pug
+        self.bot = bot
+        self.session = aiohttp.ClientSession(loop=self.bot.loop)
+        self.catapi = catapi
+        self.dogapi = dogapi
+        self.foxapi = foxapi
+        self.pugapi = pugapi
 
     @commands.command()
     @commands.cooldown(1, 60, commands.BucketType.guild)
     async def cat(self):
         """Shows a cat"""
         try:
-            async with self.session.get(self.cat) as r:
+            async with self.session.get(self.catapi) as r:
                 result = await r.json()
             await ctx.send(result['file'])
         except:
@@ -49,7 +50,7 @@ class Animal(BaseCog):
             amount = 5
         try:
             for x in range(0,amount):
-                async with self.session.get(self.cat) as r:
+                async with self.session.get(self.catapi) as r:
                     api_result = await r.json()
                     results.append(api_result['file'])
             await ctx.send("\n".join(results))
@@ -61,7 +62,7 @@ class Animal(BaseCog):
     async def dog(self):
         """Shows a dog"""
         try:
-            async with self.session.get(self.dog) as r:
+            async with self.session.get(self.dogapi) as r:
                 result = await r.json()
             await ctx.send(result['message'])
         except:
@@ -78,7 +79,7 @@ class Animal(BaseCog):
             amount = 5
         try:
             for x in range(0,amount):
-                async with self.session.get(self.dog) as r:
+                async with self.session.get(self.dogapi) as r:
                     api_result = await r.json()
                     results.append(api_result['message'])
             await ctx.send("\n".join(results))
@@ -90,7 +91,7 @@ class Animal(BaseCog):
     async def fox(self):
         """Shows a fox"""
         try:
-            async with self.session.get(self.fox) as r:
+            async with self.session.get(self.foxapi) as r:
                 result = await r.json()
             await ctx.send(result['file'])
         except:
@@ -107,7 +108,7 @@ class Animal(BaseCog):
             amount = 5
         try:
             for x in range(0,amount):
-                async with self.session.get(self.fox) as r:
+                async with self.session.get(self.foxapi) as r:
                     api_result = await r.json()
                     results.append(api_result['file'])
             await ctx.send("\n".join(results))
@@ -119,7 +120,7 @@ class Animal(BaseCog):
     async def pug(self):
         """Shows a pug"""
         try:
-            async with self.session.get(self.pug) as r:
+            async with self.session.get(self.pugapi) as r:
                 result = await r.json()
             await ctx.send(result['pug'])
         except:
@@ -136,10 +137,14 @@ class Animal(BaseCog):
             amount = 5
         try:
             for x in range(0,amount):
-                async with self.session.get(self.pug) as r:
+                async with self.session.get(self.pugapi) as r:
                     api_result = await r.json()
                     results.append(api_result['pug'])
             await ctx.send("\n".join(results))
         except:
             await ctx.send("API Error")
 
+    def __unload(self):
+        self.bot.loop.create_task(self.session.close())
+
+    __del__ = __unload
