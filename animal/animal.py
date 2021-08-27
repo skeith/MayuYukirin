5e03fc5c56f3ac9342e4bc13aca6178b3e885524
Kreus Amredes <67752638+Kreusada@users.noreply.github.com>
2021-08-27 21:23:26 +0100

    needs to be command, not group

--- animal/animal.py
+++ animal/animal.py
@@ -54,7 +54,7 @@ class Animal(BaseCog):
         except:
             await ctx.send(self.error_message)
 
-    @commands.group()
+    @commands.commands()
     @commands.cooldown(1, 60, commands.BucketType.guild)
     async def dog(self, ctx, breed: str):
         """Shows a breed of dog.
