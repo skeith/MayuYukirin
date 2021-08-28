b25ceae74c35ad024ba0eec8134aef50ef0020f5
Kreus Amredes <67752638+Kreusada@users.noreply.github.com>
2021-08-28 11:03:41 +0100

    [Animal] Add breed argument for dog command (removed pug commands)
    Dog command will now accept breed name as argument, thus pug no longer a standalone command. Use `[p]dog list` to view available breed.

--- animal/animal.py
+++ animal/animal.py
@@ -5,17 +5,18 @@ import discord
 
 # Red
 from redbot.core import commands
+from redbot.core.utils.chat_formatting import box, humanize_list
+from redbot.core.utils.menus import DEFAULT_CONTROLS, menu
 
 # Libs
 import aiohttp
 
-catapi = "https://shibe.online/api/cats"
-dogapi = "https://dog.ceo/api/breeds/image/random"
-foxapi = "http://wohlsoft.ru/images/foxybot/randomfox.php"
-pugapi = "https://dog.ceo/api/breed/pug/images/random" # Old Pug API, just in case it came back up : http://pugme.herokuapp.com/random
-roarapi = "http://randombig.cat/roar.json"
-
 BaseCog = getattr(commands, "Cog", object)
+
+
+def chunks(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
 
 
 class Animal(BaseCog):
@@ -24,11 +25,12 @@ class Animal(BaseCog):
     def __init__(self, bot):
         self.bot = bot
         self.session = aiohttp.ClientSession()
-        self.catapi = catapi
-        self.dogapi = dogapi
-        self.foxapi = foxapi
-        self.pugapi = pugapi
-        self.roarapi = roarapi
+        self.catapi = "https://shibe.online/api/cats"
+        self.dogapi = "https://dog.ceo/api/breeds/image/random"
+        self.foxapi = "http://wohlsoft.ru/images/foxybot/randomfox.php"
+        self.roarapi = "http://randombig.cat/roar.json"
+        self.dog_breed_api = "https://dog.ceo/api/breed/{}/images/random"
+        self.error_message = "An API error occured. Probably just a hiccup.\nIf this error persist for several days, please report it."
 
     @commands.command()
     @commands.cooldown(1, 60, commands.BucketType.guild)
@@ -39,7 +41,7 @@ class Animal(BaseCog):
                 result = await r.json()
             await ctx.send(result[0])
         except:
-            await ctx.send("API Error. Probably just a hiccup.\nIf this error persist for several days, please report it")
+            await ctx.send(self.error_message)
 
     @commands.command()
     @commands.cooldown(1, 120, commands.BucketType.guild)
@@ -57,36 +59,94 @@ class Animal(BaseCog):
                     results.append(api_result[0])
             await ctx.send("\n".join(results))
         except:
-            await ctx.send("API Error. Probably just a hiccup.\nIf this error persist for several days, please report it")
+            await ctx.send(self.error_message)
 
     @commands.command()
     @commands.cooldown(1, 60, commands.BucketType.guild)
-    async def dog(self, ctx):
-        """Shows a dog"""
+    async def dog(self, ctx, breed: str):
+        """Shows a breed of dog.
+        
+        Use the breed argument to display a specific breed of dog.
+        You can provide "random" for a random breed.
+        
+        You can also provide "list" for a list of all 
+        available breeds.
+        """
+        if breed.lower() == "list":
+            if not await ctx.embed_requested():
+                await ctx.send("I need to be able to send embeds to show the list.")
+                return
+            try:
+                async with self.session.get("https://dog.ceo/api/breeds/list/all") as r:
+                    result = await r.json()
+                    result = result["message"]
+            except Exception:
+                await ctx.send(self.error_message)
+            else:
+                breed_list = [i for i, _ in filter(lambda x: not bool(x[-1]), list(result.items()))]
+                embed_pages = []
+                c = list(chunks(breed_list, 10))
+                for page in c:
+                    embed = discord.Embed(
+                        title="Breeds list",
+                        description=box("\n".join(page), lang="fix"),
+                        color=await ctx.embed_colour()
+                    )
+                    embed.set_footer(text=f"Page {c.index(page)+1}/{len(c)}")
+                    embed_pages.append(embed)
+                await menu(ctx, embed_pages, DEFAULT_CONTROLS)
+
+            return
+                
+        if breed.lower() == "random":
+            api = self.dogapi
+        else:
+            api = self.dog_breed_api.format(breed)
         try:
-            async with self.session.get(self.dogapi) as r:
+            async with self.session.get(api) as r:
                 result = await r.json()
             await ctx.send(result['message'])
-        except:
-            await ctx.send("API Error. Probably just a hiccup.\nIf this error persist for several days, please report it")
+        except Exception:
+            await ctx.send(self.error_message)
 
     @commands.command()
     @commands.cooldown(1, 120, commands.BucketType.guild)
-    async def dogs(self, ctx, amount : int = 5):
+    async def dogs(self, ctx, breed: str, amount : int = 5):
         """Throws a dog bomb!
 
-        Defaults to 5, max is 10"""
+        The amount defaults to 5, max is 10.
+
+        Use the breed argument to display a specific breed of dog.
+        You can provide "random" for a random breed.
+        
+        You can also provide "list" for a list of all 
+        available breeds.
+        """
+        if not await ctx.embed_requested():
+            await ctx.send("I need to be able to send embeds for this command.")
+            return
+        if breed.lower() == "random":
+            api = self.dogapi
+        else:
+            api = self.dog_breed_api.format(breed)
         results = []
         if amount > 10 or amount < 1:
-            amount = 5
-        try:
-            for x in range(0,amount):
-                async with self.session.get(self.dogapi) as r:
+            amount = 5 
+        for x in range(0,amount):
+            try:
+                async with self.session.get(api) as r:
                     api_result = await r.json()
                     results.append(api_result['message'])
-            await ctx.send("\n".join(results))
-        except:
-            await ctx.send("API Error. Probably just a hiccup.\nIf this error persist for several days, please report it")
+            except Exception:
+                return await ctx.send(self.error_message)
+        
+        embed_pages = []
+        for i in results:
+            embed = discord.Embed(color=await ctx.embed_colour())
+            embed.set_image(url=i)
+            embed.set_footer(text=f"Page {results.index(i)+1}/{len(results)}")
+            embed_pages.append(embed)
+        await menu(ctx, embed_pages, DEFAULT_CONTROLS)
 
     @commands.command()
     @commands.cooldown(1, 60, commands.BucketType.guild)
@@ -97,7 +157,7 @@ class Animal(BaseCog):
                 result = await r.json()
             await ctx.send(result['file'])
         except:
-            await ctx.send("API Error. Probably just a hiccup.\nIf this error persist for several days, please report it")
+            await ctx.send(self.error_message)
 
     @commands.command()
     @commands.cooldown(1, 120, commands.BucketType.guild)
@@ -115,18 +175,7 @@ class Animal(BaseCog):
                     results.append(api_result['file'])
             await ctx.send("\n".join(results))
         except:
-            await ctx.send("API Error. Probably just a hiccup.\nIf this error persist for several days, please report it")
-
-    @commands.command()
-    @commands.cooldown(1, 60, commands.BucketType.guild)
-    async def pug(self, ctx):
-        """Shows a pug"""
-        try:
-            async with self.session.get(self.pugapi) as r:
-                result = await r.json()
-            await ctx.send(result['message'])
-        except:
-            await ctx.send("API Error. Probably just a hiccup.\nIf this error persist for several days, please report it")
+            await ctx.send(self.error_message)
 
     @commands.command()
     @commands.cooldown(1, 120, commands.BucketType.guild)
@@ -144,7 +193,7 @@ class Animal(BaseCog):
                     results.append(api_result['message'])
             await ctx.send("\n".join(results))
         except:
-            await ctx.send("API Error. Probably just a hiccup.\nIf this error persist for several days, please report it")
+            await ctx.send(self.error_message)
 
     @commands.command()
     @commands.cooldown(1, 60, commands.BucketType.guild)
@@ -155,7 +204,7 @@ class Animal(BaseCog):
                 result = await r.json()
             await ctx.send(result['url'])
         except:
-            await ctx.send("API Error. Probably just a hiccup.\nIf this error persist for several days, please report it")
+            await ctx.send(self.error_message)
 
     @commands.command()
     @commands.cooldown(1, 120, commands.BucketType.guild)
@@ -172,7 +221,7 @@ class Animal(BaseCog):
                     results.append(api_result['url'])
             await ctx.send("\n".join(results))
         except:
-            await ctx.send("API Error. Probably just a hiccup.\nIf this error persist for several days, please report it")
+            await ctx.send(self.error_message)
 
     def cog_unload(self):
         self.bot.loop.create_task(self.session.close())
